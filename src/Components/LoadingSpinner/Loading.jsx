9f4755327949e124e3685802cf6f1012280c82eb
Ankesh Kumar <notelyfe@gmail.com>
2023-06-04 10:57:26 +0530

    timer added to loading page

--- src/Components/LoadingSpinner/Loading.jsx
+++ src/Components/LoadingSpinner/Loading.jsx
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import loadStyle from '../Style/loading.module.css'
 
 const Loading = () => {
+
+  const [timer, setTimer] = useState(32)
+  var clearTimer
 
   useEffect(() => {
     document.body.style.overflow = "hidden"
@@ -11,11 +14,26 @@ const Loading = () => {
     }
   })
 
+  useEffect(() => {
+
+    clearTimer = setInterval(() => {
+
+      setTimer(timer - 1)
+
+    }, [1000]);
+
+    return () => clearInterval(clearTimer)
+
+  })
+
   return (
     <div className={loadStyle.loadingWrapper}>
       <div className={loadStyle.backgroundCover}></div>
       <div className={loadStyle.content}>
         <div className={loadStyle.loader}></div>
+        <p className={loadStyle.timer}>
+          {`Server will start in ${timer}s`}
+        </p>
       </div>
     </div>
   )
